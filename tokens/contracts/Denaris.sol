932d9aaa91335c2e145e5a1e2a58e3835fd6c5a0
Federico Caccia <federicoagustincaccia@gmail.com>
2021-07-24 01:20:53 -0300

    contracts deployment working fine

--- tokens/contracts/Denaris.sol
+++ tokens/contracts/Denaris.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 import "./openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 contract DENARISToken is ERC20 {
-  constructor(uint256 initialSupply) ERC20("Denaris", "DENARIS") {
+  constructor(uint256 initialSupply) ERC20("Denaris", "TDEN") {
     _mint(msg.sender, initialSupply);
   }
 }
--- tokens/migrations/2_deploy_main_contracts.js
+++ tokens/migrations/2_deploy_main_contracts.js
@@ -1,26 +1,28 @@
 const Denaris = artifacts.require("DENARISToken");
-const Resource = artifacts.require("Resource");
-const Farm = artifacts.require("Farm");
+const Resource = artifacts.require("TerraeResource");
+const Farm = artifacts.require("TerraeFarm");
 
 // Farm constructor
 // address _stakeResourceAddress, string memory _name, address[] memory resources, uint256[] memory rewards)
 
-module.exports = function(deployer, network, accounts) {
-  const denaris = deployer.deploy(Denaris, 1000000000000000000000000000); // 1B
-  const contractDenarisInstance = await denaris.deployed();
+module.exports = async (deployer, network, accounts) => {
+  await deployer.deploy(Denaris, "1000000000000000000000000000"); // 1M
+  const contractDenarisInstance = await Denaris.deployed();
 
-  const gold = await deployer.deploy(Resource, 0, "TerraeGold", "TGLD"); // 0
-  const esmerald = await deployer.deploy(Resource, 0, "TerraeEsmerald", "TESM"); // 0
-  const zaffir = await deployer.deploy(Resource, 0, "TerraeZaffir", "TZFR"); // 0
-  const ruby = await deployer.deploy(Resource, 0, "TerraeRuby", "TRBY"); // 0
+  await deployer.deploy(Resource, 0, "TerraeGold", "TGLD"); // 0
+  const contractGoldInstance = await Resource.deployed();
 
-  const contractGoldInstance = await gold.deployed();
-  const contractEsmeraldInstance = await esmerald.deployed();
-  const contractZaffirInstance = await zaffir.deployed();
-  const contractRubyInstance = await ruby.deployed();
+  await deployer.deploy(Resource, 0, "TerraeEsmerald", "TESM"); // 0
+  const contractEsmeraldInstance = await Resource.deployed();
+  
+  await deployer.deploy(Resource, 0, "TerraeZaffir", "TZFR"); // 0
+  const contractZaffirInstance = await Resource.deployed();
+  
+  await deployer.deploy(Resource, 0, "TerraeRuby", "TRBY"); // 0
+  const contractRubyInstance = await Resource.deployed();
 
-  await deployer.deploy(Farm, contractDenarisInstance, "Gold Farm", [contractGoldInstance], [40000]);
-  await deployer.deploy(Farm, contractDenarisInstance, "Esmerald Farm", [contractEsmeraldInstance], [40000]);
-  await deployer.deploy(Farm, contractDenarisInstance, "Zaffir Farm", [contractZaffirInstance], [40000]);
-  await deployer.deploy(Farm, contractDenarisInstance, "Ruby Farm", [contractRubyInstance], [40000]);
+  await deployer.deploy(Farm, contractDenarisInstance.address, "Gold Farm", [contractGoldInstance.address], [40000]);
+  await deployer.deploy(Farm, contractDenarisInstance.address, "Esmerald Farm", [contractEsmeraldInstance.address], [40000]);
+  await deployer.deploy(Farm, contractDenarisInstance.address, "Zaffir Farm", [contractZaffirInstance.address], [40000]);
+  await deployer.deploy(Farm, contractDenarisInstance.address, "Ruby Farm", [contractRubyInstance.address], [40000]);
 };
